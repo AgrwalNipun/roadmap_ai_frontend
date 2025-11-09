aff41fea84628878e6e6c57e806157d166e15755
Nipun Agrawal <nipunagrawal500@gmail.com>
2025-11-10 01:28:39 +0530

    Initial commit and created theme files for Tailwind CSS integration

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+ <h1 class="text-[hsl(var(--foreground))] text-3xl font-bold">Tailwind v4 Works!</h1>
+
+    </>
+  )
+}
+
+export default App
