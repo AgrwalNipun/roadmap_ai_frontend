69628f09890dcce8ba16c387e2a5953624b57888
Nipun Agrawal <113619474+AgrwalNipun@users.noreply.github.com>
2026-02-04 00:05:13 +0530

    Change Axios base URL to production endpoint

--- src/apis/AxiosClient.jsx
+++ src/apis/AxiosClient.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://nipun-apis.online",
 });
 
 axiosClient.interceptors.request.use((config) => {
